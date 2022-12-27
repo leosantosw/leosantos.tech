fb2a12d7a4478828ae7f7ed4c6848fc3caa34b29
leosantosw <leonardosantosx0077@gmail.com>
2022-12-27 13:30:10 -0300

    feat: create NavLink component

---
+++ components/NavLink/index.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import { Text, LinkProps as ChakraLinkProps } from '@chakra-ui/react'
+
+interface NavLinkProps extends ChakraLinkProps {
+  children: string
+  href: string
+}
+
+export function NavLink({ children, href }: NavLinkProps) {
+  return (
+    <Link as="a" href={href}>
+      <Text
+        padding="4"
+        fontSize="sm"
+        fontWeight="semibold"
+        display="inline-block"
+        color="gray.50"
+      >
+        {children}
+      </Text>
+    </Link>
+  )
+}
