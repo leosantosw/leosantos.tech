b319ac85baa2233abcfe49f602ade31542008811
leosantosw <leonardosantosx0077@gmail.com>
2022-12-28 15:24:57 -0300

    feat: implement play sound when changing theme

--- components/ToggleTheme/index.tsx
+++ components/ToggleTheme/index.tsx
@@ -1,10 +1,24 @@
 import Image from 'next/image'
+import useSound from 'use-sound'
+import { useEffect } from 'react'
 import sunImg from '../../public/assets/sun.svg'
 import moonImg from '../../public/assets/moon.svg'
 import { Button, useColorMode } from '@chakra-ui/react'
+import darkSound from '../../public/sounds/dark-active.mp3'
+import lightSound from '../../public/sounds/light-active.mp3'
 
 export function ToggleTheme() {
   const { colorMode, toggleColorMode } = useColorMode()
+  const [playDark] = useSound(darkSound)
+  const [playLight] = useSound(lightSound)
+
+  useEffect(() => {
+    if (colorMode == 'dark') {
+      playDark()
+    } else {
+      playLight()
+    }
+  }, [colorMode, playDark, playLight])
 
   return (
     <Button
