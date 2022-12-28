dd54ea6a1f1076f2a6d1b51e1878f472e6069c0e
leosantosw <leonardosantosx0077@gmail.com>
2022-12-28 03:01:50 -0300

    fix(header): separate header in a component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,11 @@
 import Link from 'next/link'
-import Image from 'next/image'
-import { NavLink } from '../components/NavLink'
-import { GradientLine } from '../components/GradientLine'
-import { Container, Flex, Text } from '@chakra-ui/react'
-import { ToggleTheme } from '../components/ToggleTheme'
+import { Text, Container } from '@chakra-ui/react'
+import { Header } from '../screens/pattens/Header'
 
 export default function Home() {
   return (
-    <Flex
-      as="header"
-      top={0}
-      width="full"
-      zIndex="docked"
-      position="sticky"
-      direction="column"
-      backgroundColor="var(header-background)"
-    >
-      <GradientLine />
-      <Container
-        width="full"
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        paddingY={8}
-        maxWidth="720px"
-      >
-        <Flex display="flex">
-          <Link href="/">
-            <div>
-              <Image
-                alt="Laura Beatris Logo"
-                src="/assets/logo.png"
-                width={50}
-                height={50}
-                priority
-              />
-            </div>
-          </Link>
-        </Flex>
-
-        <Flex>
-          <NavLink href="#">Home</NavLink>
-          <NavLink href="#">About</NavLink>
-          <NavLink href="#">Blog</NavLink>
-        </Flex>
-
-        <ToggleTheme />
-      </Container>
+    <>
+      <Header />
       <Container
         p={4}
         maxWidth="720px"
@@ -74,6 +33,6 @@ export default function Home() {
           </Link>
         </Text>
       </Container>
-    </Flex>
+    </>
   )
 }
