2ed0d43a26b31b177e39232b19890e528887f94f
leosantosw <leonardosantosx0077@gmail.com>
2022-12-28 03:56:25 -0300

    fix(NavLink): update nav link component

--- components/NavLink/index.tsx
+++ components/NavLink/index.tsx
@@ -1,19 +1,35 @@
-import Link from 'next/link'
-import { Text, LinkProps as ChakraLinkProps } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import { useRouter } from 'next/router'
+import {
+  Text,
+  LinkProps as ChakraLinkProps,
+  Link,
+  useColorMode
+} from '@chakra-ui/react'
 
 interface NavLinkProps extends ChakraLinkProps {
   children: string
   href: string
 }
 
-export function NavLink({ children, href }: NavLinkProps) {
+export function NavLink({ children, href, ...rest }: NavLinkProps) {
+  const router = useRouter()
+  const { colorMode } = useColorMode()
+
+  const isActive = router.pathname === href
+  const colorActiveMenuItem = colorMode === 'light' ? 'gray.800' : 'blue.400'
+
   return (
-    <Link as="a" href={href}>
+    <Link as={NextLink} href={href} {...rest} passHref>
       <Text
-        padding="4"
-        fontSize="sm"
-        fontWeight="semibold"
-        display="inline-block"
+        padding="8"
+        fontSize="lg"
+        fontWeight={isActive ? 'bold' : 'semibold'}
+        color={isActive ? colorActiveMenuItem : 'inherit'}
+        _hover={{
+          textDecoration: 'underline',
+          color: 'var(--chakra-colors-gray-700)'
+        }}
       >
         {children}
       </Text>
