850dbd5f95c8a76aa75557dcbd342adb7cc566bc
leosantosw <leonardosantosx0077@gmail.com>
2022-12-29 23:51:17 -0300

    feat: change color gradient line

--- components/GradientLine/index.tsx
+++ components/GradientLine/index.tsx
@@ -4,7 +4,8 @@ export function GradientLine() {
   return (
     <Box
       as="div"
-      bgGradient="linear(to-r, blue.600, blue.300)"
+      // bgGradient="linear(to-r, blue.600, blue.300)"
+      bgGradient="linear(to-l, blue.600, #7928CA)"
       h={4}
       w="full"
     />
