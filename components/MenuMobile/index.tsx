09a549bbdcb3773ceae837ca41fc6ce8cc230b97
leosantosw <leonardosantosx0077@gmail.com>
2022-12-29 21:28:30 -0300

    feat: play sound on click menu

--- components/MenuMobile/index.tsx
+++ components/MenuMobile/index.tsx
@@ -5,9 +5,15 @@ import {
   MenuButton,
   IconButton
 } from '@chakra-ui/react'
+
 import { Browser, House, Info, List } from 'phosphor-react'
 
+import useSound from 'use-sound'
+import bubbleSound from '../../public/sounds/bubble-blip.mp3'
+
 export function MenuMobile() {
+  const [playSound] = useSound(bubbleSound)
+
   return (
     <Menu>
       <MenuButton
@@ -16,6 +22,7 @@ export function MenuMobile() {
         icon={<List size={22} />}
         variant="outline"
         marginX={4}
+        onClick={() => playSound()}
       />
       <MenuList>
         <MenuItem icon={<House size={18} />}>Home</MenuItem>
