19dedfe6654671738d44985065ba1a0bffce1934
leosantosw <leonardosantosx0077@gmail.com>
2022-12-29 22:03:06 -0300

    fix: update style toggle menu

--- components/MenuMobile/index.tsx
+++ components/MenuMobile/index.tsx
@@ -6,7 +6,7 @@ import {
   IconButton
 } from '@chakra-ui/react'
 
-import { Browser, House, Info, List } from 'phosphor-react'
+import { Browser, House, Info, List, X } from 'phosphor-react'
 
 import useSound from 'use-sound'
 import bubbleSound from '../../public/sounds/bubble-blip.mp3'
@@ -15,20 +15,24 @@ export function MenuMobile() {
   const [playSound] = useSound(bubbleSound)
 
   return (
-    <Menu>
-      <MenuButton
-        as={IconButton}
-        aria-label="Options"
-        icon={<List size={22} />}
-        variant="outline"
-        marginX={4}
-        onClick={() => playSound()}
-      />
-      <MenuList>
-        <MenuItem icon={<House size={18} />}>Home</MenuItem>
-        <MenuItem icon={<Info size={18} />}>About</MenuItem>
-        <MenuItem icon={<Browser size={18} />}>Blog</MenuItem>
-      </MenuList>
+    <Menu onOpen={playSound} onClose={playSound}>
+      {({ isOpen }) => (
+        <>
+          <MenuButton
+            colorScheme="white"
+            as={IconButton}
+            aria-label="Options"
+            icon={isOpen ? <X size={20} /> : <List size={22} />}
+            variant="outline"
+            marginX={4}
+          />
+          <MenuList>
+            <MenuItem icon={<House size={18} />}>Home</MenuItem>
+            <MenuItem icon={<Info size={18} />}>About</MenuItem>
+            <MenuItem icon={<Browser size={18} />}>Blog</MenuItem>
+          </MenuList>
+        </>
+      )}
     </Menu>
   )
 }
