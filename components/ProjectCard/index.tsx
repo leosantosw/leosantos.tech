79f0304cdb3427521cf5bb0c8124c344e420b276
leosantosw <leonardosantosx0077@gmail.com>
2023-01-07 21:05:34 -0300

    feat: play sound on hover card

--- components/ProjectCard/index.tsx
+++ components/ProjectCard/index.tsx
@@ -7,6 +7,8 @@ import {
   useColorMode
 } from '@chakra-ui/react'
 import Link from 'next/link'
+import useSound from 'use-sound'
+import biteSound from '../../public/sounds/bite.mp3'
 
 type ProjectCardProps = {
   title: string
@@ -17,24 +19,30 @@ type ProjectCardProps = {
 export function ProjectCard({ title, description, link }: ProjectCardProps) {
   const { colorMode } = useColorMode()
   const isDarkMode = colorMode === 'dark'
+  const [play] = useSound(biteSound)
+
+  function handlePlaySound() {
+    play()
+  }
 
   return (
     <Link href={link} passHref target="_blank">
       <Card
+        onMouseEnter={handlePlaySound}
         direction="row"
         overflow="hidden"
         variant="outline"
         borderColor={isDarkMode ? 'inherit' : 'blue.600'}
         _hover={{
           boxShadow: 'lg',
-          transform: 'translateY(-4px)',
-          transition: 'all 0.2s'
+          transform: 'scale(1.02)',
+          transition: 'all .5s'
         }}
       >
         <Stack>
           <CardBody>
             <Heading size="md">{title}</Heading>
-            <Text py="2">{description}</Text>
+            <Text py="2.5">{description}</Text>
           </CardBody>
         </Stack>
       </Card>
