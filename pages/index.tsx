a84f619f9b9006058b9cea093dee5ac2877ddc93
leosantosw <leonardosantosx0077@gmail.com>
2023-01-07 21:54:28 -0300

    feat: add footer on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,22 @@ import { Header } from '../screens/pattens/Header'
 import { Projects } from '../screens/pattens/Projects'
 import { ContentWrapper } from '../screens/pattens/ContentWrapper'
 
+import { Footer } from '../components/Footer'
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Leo Santos - Software Engineer</title>
       </Head>
+
       <Header />
 
       <ContentWrapper />
 
       <Projects />
+
+      <Footer />
     </>
   )
 }
